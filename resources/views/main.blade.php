83d43d040579a154b7342d7bb0f265d5f2fc95e7
Ivan <145771734+IvanGricev@users.noreply.github.com>
2025-05-07 09:58:51 +0300

    Временное подключение бутстрап по cdn

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,6 +6,7 @@
     <title>Bloomon</title>
     <!-- Подключение стилей -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body>
     <!-- Шапка сайта -->
@@ -25,5 +26,6 @@
 
     <!-- Подключение скриптов -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
