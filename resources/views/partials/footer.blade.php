b3b01591f66dc70b0fd1142dfc2163866a48879c
Daxa <149832298+DaxaAAA@users.noreply.github.com>
2025-05-23 00:30:42 +0300

    Сделала футер

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,13 +1,28 @@
-<div class="footer-links">
-    <div class="row">
-        <div class="col-md-3">
-            <h5>О компании</h5>
-            <ul class="list-unstyled">
+<link rel="stylesheet" href="/css/footer.css">
+<footer class="site-footer">
+    <div class="footer-container">
+        <div class="footer-col">
+            <div class="footer-logo">
+                <img src="/images/logo.svg" alt="Bloomon" height="32">
+            </div>
+            <div class="footer-desc">Цветы с доставкой по городу и области. Каждый день — повод для радости!</div>
+        </div>
+        <div class="footer-col">
+            <h5>Компания</h5>
+            <ul>
                 <li><a href="{{ route('about') }}">О нас</a></li>
                 <li><a href="{{ route('contact') }}">Контакты</a></li>
-                <li><a href="{{ route('support.create') }}">Обратиться в поддержку</a></li>
+                <li><a href="{{ route('support.create') }}">Поддержка</a></li>
             </ul>
         </div>
-        <!-- ... остальные колонки ... -->
+        <div class="footer-col">
+            <h5>Каталог</h5>
+            <ul>
+                <li><a href="{{ route('products.index') }}">Букеты</a></li>
+                <li><a href="{{ route('subscriptions.index') }}">Подписки</a></li>
+                <li><a href="{{ route('blog.index') }}">Блог</a></li>
+            </ul>
+        </div>
     </div>
-</div> 
+    <div class="footer-bottom">© {{ date('Y') }} Bloomon. Все права защищены.</div>
+</footer> 
